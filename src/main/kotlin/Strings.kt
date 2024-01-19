0511fd22d507ecf953f908eb6ef9074169806c33
Mark Munene <nmmunene@gmail.com>
2024-01-19 22:47:20 +0300

    First Commit

---
+++ src/main/kotlin/Strings.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() {
+    string1()
+}
+
+fun string1() {
+    val string = "I Am Groot"
+    println("Our string is: ${string.uppercase().plus(" and these are my friends")}") //plus or +
+
+    val name = "mark"
+    println("Reversed Caps: ${name.uppercase().reversed()}")
+
+
+}
